a510b9524dfd571afe75d69980adc939f21449af
SilmiYu <silmiyushini19@gmail.com>
2024-05-29 09:37:43 +0700

    upgrade + app versi 3

--- index.php
+++ index.php
@@ -9,15 +9,17 @@
 
 <!DOCTYPE html>
 <html lang="en">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aplikasi Perpustakaan</title>
-    <link rel="stylesheet" href="stylehome.css">
+
 </head>
 <body>
     <div class="judul">
-        <h1>Aplikasi Perpustakaan</h1>
+        <h1>Aplikasi Perpustakaan v.2</h1>
 
         <div class="menu">
             <a href="./buku.php">Lihat daftar buku</a>
